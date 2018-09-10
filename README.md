1573e1a37ea01cb93b08041ec06f9a6a0fe90d26
R-Layer <layer@europe.com>
2018-09-10 11:53:35 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Start project for the official Layer's portfolio.
+This file will be updated once the project is completed.
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import "./css/App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Portfolio v0.1</h1>
+        </header>
+        <p className="App-intro">First paragraph</p>
+      </div>
+    );
+  }
+}
+
+export default App;
